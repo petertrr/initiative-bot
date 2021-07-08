884a87d861524995de79111f194820337a145dd6
petertrr <peter.trifanov@mail.ru>
2021-07-08 10:00:04 +0300

    Use logback for logging
    ### What's done:
    * Added dependency
    * Added configuration

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,4 +4,6 @@ object Versions {
     const val junit = "5.7.1"
     const val mockito = "3.11.2"
     const val discord4j = "3.1.5"
+    const val slf4j = "1.7.30"
+    const val logback = "1.2.3"
 }
