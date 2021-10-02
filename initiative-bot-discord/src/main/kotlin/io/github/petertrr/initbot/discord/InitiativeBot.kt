679e5043b327f95c08d99b1b1b5bd0348ee9b9dd
Peter Trifanov <peter.trifanov@mail.ru>
2021-10-02 21:54:53 +0300

    feat: Refactor InitiativeBot.kt, add ResponseProcessor.kt (#6)
    * Organize context of channels into `ChannelState`
    * Implement `ResponseProcessor`, which reacts at `CommandResult`s using `ResponseContext` of the channel
    * Separate `CommandResult` handling from side-effects on discord-bot side

--- initiative-bot-discord/src/main/kotlin/io/github/petertrr/initbot/discord/InitiativeBot.kt
+++ initiative-bot-discord/src/main/kotlin/io/github/petertrr/initbot/discord/InitiativeBot.kt
@@ -1,5 +1,6 @@
 package io.github.petertrr.initbot.discord
 
+import discord4j.common.util.Snowflake
 import discord4j.core.DiscordClient
 import discord4j.core.`object`.entity.Message
 import discord4j.core.`object`.entity.User
@@ -8,24 +9,22 @@ import discord4j.core.event.domain.message.MessageCreateEvent
 import discord4j.rest.util.AllowedMentions
 import io.github.petertrr.initbot.*
 import io.github.petertrr.initbot.discord.entities.BotConfiguration
-import io.github.petertrr.initbot.discord.entities.UserState
+import io.github.petertrr.initbot.discord.entities.ChannelState
+import io.github.petertrr.initbot.discord.entities.ResponseContext
 import io.github.petertrr.initbot.sorting.DescendantSorter
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactor.mono
 import mu.KotlinLogging
 import reactor.core.Disposable
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 import java.time.Duration
 import java.util.concurrent.ConcurrentHashMap
 
 private val logger = KotlinLogging.logger {}
 class InitiativeBot(private val botConfiguration: BotConfiguration) {
     private lateinit var client: DiscordClient
-    private val initiatives = mutableMapOf<String, Initiative>()
-    private val userNameByCharacterNameByChannel = ConcurrentHashMap<String, MutableMap<User, UserState>>()
+    private val initiatives = ConcurrentHashMap<Snowflake, Initiative>()
+    private val responseProcessors = ConcurrentHashMap<Snowflake, ResponseProcessor>()
+    private val channelStates = ConcurrentHashMap<Snowflake, ChannelState>()
     private lateinit var countdownSubscription: Disposable
 
     fun start(args: List<String>) {
@@ -35,41 +34,52 @@ class InitiativeBot(private val botConfiguration: BotConfiguration) {
             gatewayDiscordClient.on(MessageCreateEvent::class.java)
                 .filter { it.message.content.startsWith(botConfiguration.prefix) }
                 .flatMap {
-                    initializeInitiativeIfAbsent(it.message.channelId.asString())
+                    initializeInitiativeIfAbsent(it.message.channelId)
                     handleMessageCreateEvent(it)
                 }
         }
             .block()
     }
 
-    private fun initializeInitiativeIfAbsent(channelId: String) {
+    private fun initializeInitiativeIfAbsent(channelId: Snowflake) {
         initiatives.computeIfAbsent(channelId) {
-            logger.info("Creating new Initiative for channel $it")
+            logger.info { "Creating new Initiative for channel $it" }
             Initiative(
                 sorter = DescendantSorter(),
                 turnDurationSeconds = botConfiguration.turnDurationSeconds
             )
         }
-        userNameByCharacterNameByChannel.computeIfAbsent(channelId) {
-            mutableMapOf()
+        channelStates.computeIfAbsent(channelId) {
+            ChannelState(channelId)
         }
-    }
-
-    private fun getCharacterNameFor(user: User, channelId: String): String {
-        val characterNames = userNameByCharacterNameByChannel[channelId]!![user]
-            ?.characterNames
-        return characterNames
-            ?.singleOrNull()
-            ?: user.username.also {
-                logger.warn {
-                    "User $it has ${if (characterNames == null) "no" else "multiple ($characterNames)"} added characters, so will use username as fallback"
+        responseProcessors.computeIfAbsent(channelId) {
+            ResponseProcessor.create {
+                doOn(AddSuccess::class) {
+                    if (it.name !in authorState.characterNames) {
+                        authorState.characterNames.add(it.name)
+                    }
+                }
+                doOn(RemoveSuccess::class) { commandResult ->
+                    // todo: if user attempts to remove other user's character, they will succeed in initiative-bot-core, but fail here
+                    authorState.characterNames.removeIf { it == commandResult.name }
+                }
+                doOn(EndSuccess::class) {
+                    cleanup(channelId)
+                }
+                mapIf(RollResult::class) {
+                    logger.info("@${author.username} rolled ${it.roll}")
+                    "${author.mention} rolled `${it.roll} + (${it.modifier}) = ${it.total}` for character ${it.name}"
+                }
+                mapIf(RoundResult::class) { formatRoundMessage(it, channelId) }
+                mapIf(CountdownStarted::class) { commandResult ->
+                    val user = channelState.userStates.filterValues {
+                        commandResult.combatant.name in it.characterNames
+                    }.keys.single()
+                    "${commandResult.combatant.name} (init ${commandResult.combatant.currentInitiative}) is up, ${user.mention}, you have ${commandResult.period} seconds for your turn!".let {
+                        if (isLastCombatant()) "$it Also, you are the last in this round, DM should call `end-round` after." else it
+                    }
                 }
             }
-    }
-
-    private fun computeUserStateIfAbsent(user: User, channelId: String): UserState {
-        return userNameByCharacterNameByChannel[channelId]!!.computeIfAbsent(user) {
-            UserState()
         }
     }
 
@@ -78,56 +88,30 @@ class InitiativeBot(private val botConfiguration: BotConfiguration) {
         val channelId = message.channelId
         val author = message.author.get()
         val rawCommand = message.content.dropWhile { it in botConfiguration.prefix }.trim()
-        val initiative = initiatives[channelId.asString()]!!
+        val initiative = initiatives[channelId]!!
+        val channelState = channelStates[channelId]!!
+        val responseProcessor = responseProcessors[channelId]!!
         logger.info { "Received message `${message.content}` from `${author.username}`, will run command `$rawCommand`" }
 
         val result = try {
-            val fallbackName = getCharacterNameFor(author, channelId.asString())
+            val fallbackName = getCharacterNameFor(author, channelId)
             initiative.execute(rawCommand, fallbackName)
         } catch (e: Exception) {
             logger.error("Error executing command", e)
-            Failure(e)
+            Failure(e, rawCommand)
         }
 
-        val response: String = when (result) {
-            is Success -> result.message
-            is AddSuccess -> {
-                val userState = computeUserStateIfAbsent(author, channelId.asString())
-                if (result.name !in userState.characterNames) {
-                    userState.characterNames.add(result.name)
-                }
-                result.message
+        val response = responseProcessor.process(
+            ResponseContext(author, channelState, initiative),
+            result
+        )
+
+        if (result is CountdownStarted) {
+            if (::countdownSubscription.isInitialized && !countdownSubscription.isDisposed) {
+                logger.info { "Disposing countdown subscription, because it's the next combatant's turn" }
+                countdownSubscription.dispose()
             }
-            is RemoveSuccess -> {
-                // todo: if user attempts to remove other user's character, they will succeed in initiative-bot-core, but fail here
-                val userState = computeUserStateIfAbsent(author, channelId.asString())
-                userState.characterNames.removeIf { it == result.name }
-                result.message
-            }
-            is RollResult -> {
-                if (::countdownSubscription.isInitialized && !countdownSubscription.isDisposed) {
-                    logger.info { "Disposing countdown subscription, because ${author.username} has started rolling for new round" }
-                    countdownSubscription.dispose()
-                }
-                logger.info("@${author.username} rolled ${result.roll}")
-                "${author.mention} rolled `${result.roll} + (${result.modifier}) = ${result.total}` for character ${result.name}"
-            }
-            is Failure -> "Error during command `$rawCommand`: `${result.t.javaClass.simpleName}: ${result.t.message}`"
-            is CountdownStarted -> {
-                val user = userNameByCharacterNameByChannel[channelId.asString()]!!.filterValues {
-                    result.combatant.name in it.characterNames
-                }.keys.single()
-                "${result.combatant.name} (init ${result.combatant.currentInitiative}) is up, ${user.mention}, you have ${result.period} seconds for your turn!".let {
-                    if (initiative.hasNextCombatant()) it else "$it Also, you are the last in this round, DM should call `end-round` after."
-                }.also {
-                    countdownSubscription = message.channel.startCountdown(author, result.period).subscribe()
-                }
-            }
-            is RoundResult -> formatRoundMessage(result, channelId.asString())
-            is EndSuccess -> {
-                cleanup(channelId.asString())
-                result.message
-            }
+            countdownSubscription = message.channel.startCountdown(result.period).subscribe()
         }
 
         return message.channel.flatMap { messageChannel ->
@@ -138,7 +122,19 @@ class InitiativeBot(private val botConfiguration: BotConfiguration) {
         }
     }
 
-    private fun Mono<MessageChannel>.startCountdown(author: User, seconds: Long): Flux<Message> {
+    private fun getCharacterNameFor(user: User, channelId: Snowflake): String {
+        val characterNames = channelStates[channelId]!!.userStates[user]
+            ?.characterNames
+        return characterNames
+            ?.singleOrNull()
+            ?: user.username.also {
+                logger.warn {
+                    "User $it has ${if (characterNames == null) "no" else "multiple ($characterNames)"} added characters, so will use username as fallback"
+                }
+            }
+    }
+
+    private fun Mono<MessageChannel>.startCountdown(seconds: Long): Flux<Message> {
         if (::countdownSubscription.isInitialized && !countdownSubscription.isDisposed) {
             logger.info("Canceling the previous countdown")
             countdownSubscription.dispose()
@@ -150,7 +146,6 @@ class InitiativeBot(private val botConfiguration: BotConfiguration) {
                 .take(numIntervals)
                 .flatMap { i ->
                     messageChannel.createMessage {
-                        it.setAllowedMentions(AllowedMentions.builder().allowUser(author.id).build())
                         if (i + 1 < numIntervals) {
                             it.setContent("${(numIntervals - i - 1) * botConfiguration.turnDurationSeconds} seconds left!")
                         } else {
@@ -162,13 +157,13 @@ class InitiativeBot(private val botConfiguration: BotConfiguration) {
             .log()
     }
 
-    private fun formatRoundMessage(roundResult: RoundResult, channelId: String): String {
+    private fun formatRoundMessage(roundResult: RoundResult, channelId: Snowflake): String {
         val longestLineLength = roundResult.combatants.maxOf {
             it.name.length + it.currentInitiative!!.toString().length
         }
         return "Starting **round ${roundResult.roundIdx}** of initiative:" +
                 roundResult.combatants.joinToString(System.lineSeparator(), prefix = System.lineSeparator()) { combatant ->
-                    val owner = userNameByCharacterNameByChannel[channelId]!!.filterValues {
+                    val owner = channelStates[channelId]!!.userStates.filterValues {
                         combatant.name in it.characterNames
                     }.keys.single()
                     combatant.name + ".".repeat(longestLineLength - combatant.name.length) + combatant.currentInitiative +
@@ -176,10 +171,10 @@ class InitiativeBot(private val botConfiguration: BotConfiguration) {
                 }
     }
 
-    private fun cleanup(channelId: String) {
+    private fun cleanup(channelId: Snowflake) {
         logger.info { "Removing initiative for channel $channelId" }
         initiatives.remove(channelId)
         logger.info { "Clearing users' character names for channel $channelId" }
-        userNameByCharacterNameByChannel[channelId]!!.clear()
+        channelStates[channelId]!!.userStates.clear()
     }
 }
