61906db549a8fe19f9365303845cc441c0314c6b
Peter Trifaniov <peter.trifanov@mail.ru>
2021-12-18 12:31:38 +0300

    chore: Enable gradle configuration cache
    * Don't create `~/init.gradle`
    * Use pre-configured build cache URL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,10 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include("initiative-bot-core")
 include("initiative-bot-discord")
+
+buildCache {
+    remote<HttpBuildCache> {
+        url = uri("http://localhost:3696/")
+        isAllowInsecureProtocol = true
+    }
+}
